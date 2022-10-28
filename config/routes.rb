73ed7f914c62bf2fe0aa0909586c8005f79b97d8
justynapaldyna <justyna.paldyna@gmail.com>
2022-10-28 11:41:45 +0200

    [7,8] only admin can add,edit,delete movies + set up of admin panel

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
+  
+  authenticated :user, ->(user) { user.admin? } do
+    get 'admin', to: 'admin#index'
+    get 'admin/index'
+    get 'admin/users'
+    get 'admin/movies'
+    get 'admin/show_movie'
+    get 'admin/show_user'
+    get 'admin/edit_user'
+  end
+  
   resources :categories
   resources :movies
   get 'home/index'
@@ -6,5 +17,5 @@ Rails.application.routes.draw do
   root to: "home#index"
   devise_for :users
   resources :users
-  
+
 end
