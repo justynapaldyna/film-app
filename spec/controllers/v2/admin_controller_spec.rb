b6b9018a528d55158869dfc821e7ffe9612a9160
Piotr Aleksandrowicz <p.aleksandrowicz@binarapps.com>
2023-01-25 11:03:24 +0100

    tests

---
+++ spec/controllers/v2/admin_controller_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe V2::AdminController, type: :controller do
+
+    describe 'GET users method' do
+      let!(:users) { create_list(:user, 10) }
+
+      subject { get :users }
+
+      before { subject }
+
+      it { expect(response.status).to eq(200) }
+
+      it 'shows all the users' do
+        expect(JSON.parse(response.body)['data'].map { |obj| obj['id'].to_i }).to eq(User.all.ids)
+      end
+
+      # context 'when data is invalid' do
+      #   subject { get :users, params: { id: -1 } }
+
+      #   it { expect(response.status).to eq(404) }
+      # end
+    end
+end
