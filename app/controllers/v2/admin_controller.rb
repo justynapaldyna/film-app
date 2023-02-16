824903919c5a219861251daf77beeb3ee11edab2
justynapaldyna <justyna.paldyna@gmail.com>
2023-02-16 15:03:12 +0100

    responding with 400

--- app/controllers/v2/admin_controller.rb
+++ app/controllers/v2/admin_controller.rb
@@ -1,4 +1,7 @@
+require 'active_record/errors'
 class V2::AdminController < ApplicationController
+
+  rescue_from StandardError, :with => :render_400
   
   def users
     users = User.all
@@ -9,6 +12,7 @@ class V2::AdminController < ApplicationController
     movies = Movie.all 
     render json: movies, each_serializer: MovieAdminSerializer
   end
+ 
 end
   
     
--- spec/controllers/v2/admin_controller_spec.rb
+++ spec/controllers/v2/admin_controller_spec.rb
@@ -5,13 +5,7 @@ RSpec.describe V2::AdminController, type: :controller do
     describe 'GET users method' do
       let!(:users) { create_list(:user, 10) }
 
-      subject { get :users }
-
-        context 'when data is invalid' do
-          subject { get :users, params: { id: (-1) } }
-
-          it { expect(response.status).to eq(404) }
-        end
+      subject { get :users } 
 
       before { subject }
 
@@ -19,6 +13,13 @@ RSpec.describe V2::AdminController, type: :controller do
 
       it 'shows all the users' do
         expect(JSON.parse(response.body)['data'].map { |obj| obj['id'].to_i }).to eq(User.all.ids)
+      end
+
+      context 'when data is invalid' do
+        
+        subject { get :users, params: { id: -1 } }
+
+        it { expect(response.status).to eq(400) }
       end
 
         
